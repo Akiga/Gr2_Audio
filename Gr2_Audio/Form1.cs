53ab0a401fd13c11c5efc9ab83e1ad7d23dd471e
Zang65 <138585030+Zang65@users.noreply.github.com>
2025-06-15 01:08:52 +0700

    Thêm thành phần giúp người dùng dễ dàng thao tác và theo dõi cuộc gọi Form1.cs

--- Gr2_Audio/Form1.cs
+++ Gr2_Audio/Form1.cs
@@ -129,6 +129,91 @@ namespace Gr2_Audio
             ForeColor = Color.White,
             Checked = true
         };
+        TextBox ipAddressTextBox = new TextBox
+        {
+            Location = new Point(50, 100),
+            Name = "ipAddressTextBox",
+            Size = new Size(400, 30),
+            Font = new Font("Arial", 12),
+            ForeColor = Color.Gray,
+            Text = "Enter IP address to connect"
+        };
+        
+        Button connectButton = new Button
+        {
+            Location = new Point(50, 150),
+            Name = "connectButton",
+            Size = new Size(120, 50),
+            Text = "Connect",
+            BackColor = Color.LightGreen,
+            Font = new Font("Arial", 12, FontStyle.Bold),
+            FlatStyle = FlatStyle.Flat
+        };
+        
+        Button endButton = new Button
+        {
+            Location = new Point(190, 150),
+            Name = "endButton",
+            Size = new Size(120, 50),
+            Text = "End Call",
+            BackColor = Color.IndianRed,
+            Enabled = false,
+            Font = new Font("Arial", 12, FontStyle.Bold),
+            FlatStyle = FlatStyle.Flat
+        };
+        
+        Button muteButton = new Button
+        {
+            Location = new Point(330, 150),
+            Name = "muteButton",
+            Size = new Size(120, 50),
+            Text = "Mute Mic",
+            BackColor = Color.LightGray,
+            Enabled = false,
+            Font = new Font("Arial", 12, FontStyle.Bold),
+            FlatStyle = FlatStyle.Flat
+        };
+        
+        Label statusLabel = new Label
+        {
+            Location = new Point(50, 220),
+            Name = "statusLabel",
+            Size = new Size(400, 60),
+            Text = "Status: Ready",
+            Font = new Font("Arial", 12, FontStyle.Bold),
+            ForeColor = Color.White,
+            TextAlign = ContentAlignment.MiddleCenter
+        };
+        
+        Label volumeTitle = new Label
+        {
+            Location = new Point(50, 290),
+            Text = "Volume:",
+            Size = new Size(70, 30),
+            Font = new Font("Arial", 12),
+            ForeColor = Color.White
+        };
+        
+        TrackBar volumeBar = new TrackBar
+        {
+            Location = new Point(120, 290),
+            Name = "volumeBar",
+            Size = new Size(250, 45),
+            Minimum = 0,
+            Maximum = 100,
+            Value = 50,
+            TickFrequency = 10
+        };
+        
+        Label volumeLabel = new Label
+        {
+            Location = new Point(380, 290),
+            Name = "volumeLabel",
+            Size = new Size(50, 30),
+            Text = "50%",
+            Font = new Font("Arial", 12),
+            ForeColor = Color.White
+        };
 
         
     }
